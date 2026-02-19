abacca1469fcb471c5834c358ece41ae217994a1
AntonDor <dorogovanton291@gmail.com>
2026-02-20 00:48:47 +0300

    add models

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -2,14 +2,15 @@ package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
 import lombok.Data;
+
 import java.time.LocalDate;
 
 @Data
 @Builder
-public class Film {
+public class User {
     private long id;
+    private String email;
+    private String login;
     private String name;
-    private String description;
-    private LocalDate releaseDate;
-    private double duration;
+    private LocalDate birthday;
 }
